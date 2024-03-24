67d074c730b93a24f0a0ccdae7e96f3d76dd6e2d
anthonyharrison <anthony.p.harrison@gmail.com>
2024-03-24 17:18:31 +0000

    feat: refactor processing

--- lib4vex/csaf/csaf_generator.py
+++ lib4vex/csaf/csaf_generator.py
@@ -12,12 +12,12 @@ class CSAFVEXGenerator:
         self.creator = creator
         self.csaf_gen = CSAFGenerator()
 
-    def generate_csaf(self, vulnerabilities, metadata, product, components):
+    def generate_csaf(self, vulnerabilities, metadata, product):
         csaf_vuln = Vulnerability()
         self.csaf_gen.set_title("Technical Summary")
         self.csaf_gen.set_value("author", self.author)
         # Product
-        self.csaf_gen.add_product(product_name=product['name'], vendor=self.author, release=product['version'])
+        self.csaf_gen.add_product(product_name=product['name'], vendor=self.author, release=product['version'], sbom=product['sbom'])
         if len(metadata) > 0:
             # Extract attributes
             for attribute in metadata.keys():
@@ -30,41 +30,28 @@ class CSAFVEXGenerator:
         for vulnerability in vulnerabilities:
             csaf_vuln.initialise()
             csaf_vuln.copy_vulnerability(vulnerability)
-            # Validate component against SBOM if available
             product_name = csaf_vuln.get_value("product")
             release = csaf_vuln.get_value("release")
-            if len(components) > 0:
-                # Validation against SBOM
-                valid_component = False
-                for component in components:
-                    name=component['name']
-                    version=component['version']
-                    if name==product_name and version==release:
-                        # Component found in SBOM
-                        valid_component=True
-                        break
-            else:
-                # No component validation against SBOM
-                valid_component=True
-            if valid_component:
-                # Add product
-                self.csaf_gen.add_product(
-                    product_name=csaf_vuln.get_value("product"),
-                    vendor=csaf_vuln.get_value("vendor"),
-                    release=csaf_vuln.get_value("release")
-                )
-                # Add vulnerability
-                self.csaf_gen.add_vulnerability(
-                    product_name=product_name,
-                    release=release,
-                    id=csaf_vuln.get_value("id"),
-                    description=csaf_vuln.get_value("description"),
-                    status=csaf_vuln.get_value("status"),
-                    comment=csaf_vuln.get_value("comment"),
-                    justification=csaf_vuln.get_value("justification")
-                )
-            else:
-                print (f"[ERROR] Vulnerability {csaf_vuln.get_value('id')} not processed. {product_name} - {release} not found in SBOM")
+            # Add product
+            self.csaf_gen.add_product(
+                product_name=product_name,
+                vendor=csaf_vuln.get_value("vendor"),
+                release=release,
+            )
+            # Add vulnerability
+            self.csaf_gen.add_vulnerability(
+                product_name=product_name,
+                release=release,
+                id=csaf_vuln.get_value("id"),
+                description=csaf_vuln.get_value("description"),
+                status=csaf_vuln.get_value("status"),
+                comment=csaf_vuln.get_value("comment"),
+                justification=csaf_vuln.get_value("justification"),
+                created=csaf_vuln.get_value("created")
+            )
         self.csaf_gen.generate_csaf()
     def get_document(self):
         return self.csaf_gen.csaf_document
+
+    def get_revision(self):
+        return self.csaf_gen.get_revision()
