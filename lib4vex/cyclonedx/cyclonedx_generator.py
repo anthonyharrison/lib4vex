aacf6d1e15439e5d53818d9d34099bd07adca2a9
anthonyharrison <anthony.p.harrison@gmail.com>
2024-03-24 17:19:07 +0000

    feat: Update CycloneDX VEX generation

--- lib4vex/cyclonedx/cyclonedx_generator.py
+++ lib4vex/cyclonedx/cyclonedx_generator.py
@@ -3,6 +3,8 @@
 
 from lib4sbom.generator import SBOMGenerator
 from lib4sbom.sbom import SBOM
+from lib4sbom.data.vulnerability import Vulnerability
+from lib4sbom.data.package import SBOMPackage
 from lib4vex.version import VERSION
 
 class CycloneDXVEXGenerator:
@@ -16,13 +18,23 @@ class CycloneDXVEXGenerator:
         self.creator = creator
         self.sbom = SBOM()
         self.sbom_document = SBOMGenerator(format='json', sbom_type='cyclonedx', application="lib4vex", version=VERSION)
+        self.revision = 1
 
-    def generate_cyclonedx(self, vulnerabilities, project_name, components):
+    def generate_cyclonedx(self, vulnerabilities, project_name, metadata):
+        self.revision = metadata.get("bom_version", "0")
+        self.sbom.set_bom_version(int(self.revision)+1)
+        if metadata.get("property") is not None:
+            for p in metadata.get("property"):
+                self.sbom.set_property(p['name'], p['value'])
+        self.sbom.set_property(f"Revision_{int(self.revision)+1}",metadata.get("revision_reason","Initial version"))
         self.sbom.add_vulnerabilities(vulnerabilities)
         self.sbom_document.generate(project_name=project_name, sbom_data = self.sbom.get_sbom(), send_to_output=False)
 
     def get_document(self):
         return self.sbom_document.get_sbom()
+
+    def get_revision(self):
+        return self.revision
 
 
 
