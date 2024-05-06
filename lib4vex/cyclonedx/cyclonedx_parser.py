bd859bb53575f0f76f982876bf1d19b783ba20ce
anthonyharrison <anthony.p.harrison@gmail.com>
2024-05-06 20:05:03 +0100

    fix: Parsing metadata and product (CycloneDX)

--- lib4vex/cyclonedx/cyclonedx_parser.py
+++ lib4vex/cyclonedx/cyclonedx_parser.py
@@ -11,16 +11,26 @@ class CycloneDXVEXParser:
 
     def parse(self, vex_file):
         """parses CycloneDX file extracting vulnerability information"""
-        if vex_file.endswith("json"):
+        if vex_file.endswith(".json"):
             return self.parse_cyclonedx_json(vex_file)
         else:
             return None
 
     def parse_cyclonedx_json(self, filename):
         cdx_parser = SBOMParser(sbom_type="cyclonedx")
-        # Load SBOM - will autodetect SBOM type
+        # Load SBOM - will detect SBOM type
         cdx_parser.parse_file(filename)
-        product = cdx_parser.get_packages()
+        #product = cdx_parser.get_packages()
+        product = []
         metadata = cdx_parser.get_document()
+
+        if "creator" in metadata:
+            for c in metadata["creator"]:
+                if c[0] == "person":
+                    name = c[1].split("#")
+                    metadata["supplier"] = name[0]
+                    metadata["supplier_url"] = name[1]
+        if "name" in metadata:
+            product.append({"name": metadata["name"], "version" : metadata.get("metadata_version","")})
 
         return metadata, product, cdx_parser.get_vulnerabilities()
