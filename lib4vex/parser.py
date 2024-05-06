8c47c8a385721aaae717ef8384083f573502e467
anthonyharrison <anthony.p.harrison@gmail.com>
2024-05-06 20:07:44 +0100

    feat: auto detect type of vex document

--- lib4vex/parser.py
+++ lib4vex/parser.py
@@ -10,19 +10,15 @@ from lib4vex.spdx.spdx_parser import SPDXVEXParser
 
 class VEXParser:
 
-    def __init__(self, vex_type = "openvex"):
+    vex_options = {"csaf": CSAFVEXParser(), "cyclonedx": CycloneDXVEXParser(), "openvex": OpenVEXParser(), "spdx": SPDXVEXParser()}
+
+    def __init__(self, vex_type = "auto"):
         self.vex_type = vex_type.lower()
-        if self.vex_type not in ["openvex", "cyclonedx", "csaf", "spdx"]:
+        if self.vex_type not in ["openvex", "cyclonedx", "csaf", "spdx", "auto"]:
             # Set a default SBOM type
-            self.vex_type = "openvex"
-        if self.vex_type == "openvex":
-            self.vex = OpenVEXParser()
-        elif vex_type == "cyclonedx":
-            self.vex = CycloneDXVEXParser()
-        elif vex_type == "spdx":
-            self.vex = SPDXVEXParser()
-        else:
-            self.vex = CSAFVEXParser()
+            self.vex_type = "auto"
+        if self.vex_type != "auto":
+            self.vex = self.vex_options[self.vex_type]
         self.vex_complete = False
         self.debug = os.getenv("LIB4VEX_DEBUG") is not None
         self.metadata = {}
@@ -30,7 +26,26 @@ class VEXParser:
         self.vulnerabilities = []
 
     def parse(self, filename):
-        self.metadata, self.product, self.vulnerabilities = self.vex.parse(filename)
+        if self.debug:
+            print (f"Parse {filename} of VEX Type {self.vex_type}")
+        if self.vex_type == "auto":
+            if self.debug:
+                print (f"VEX file type detection for {filename}")
+            for vex_type in self.vex_options:
+                if self.debug:
+                    print (f"Checking {filename} type : {vex_type}")
+                self.vex = self.vex_options[vex_type]
+                self.metadata, self.product, self.vulnerabilities = self.vex.parse(filename)
+                if len(self.metadata) > 0:
+                    # Assume found type of VEX document
+                    self.vex_type = vex_type
+                    break
+            if len(self.metadata) == 0:
+                print ("[ERROR] Unable to determine type of VEX document")
+                self.product = []
+                self.vulnerabilities = []
+        else:
+            self.metadata, self.product, self.vulnerabilities = self.vex.parse(filename)
 
     def get_type(self):
         return self.vex_type
