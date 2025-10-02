557008b67d50a1c19a040c8be9e90c976798d5da
anthonyharrison <anthony.p.harrison@gmail.com>
2025-10-02 11:28:25 +0100

    fix: cater for partial product names

--- lib4vex/generator.py
+++ lib4vex/generator.py
@@ -107,6 +107,10 @@ class VEXGenerator:
                         # Component found in SBOM
                         valid_component = True
                         break
+                    elif product_name in name and release in version:
+                        # Part of component name found in SBOM
+                        valid_component = True
+                        break
             else:
                 # No component validation against SBOM
                 valid_component = True
